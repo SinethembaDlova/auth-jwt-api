096e9742f27d372cbf906952d8e4fbf01d367a4f
Sinethemba <sinethemba@wealthmigrate.com>
2022-08-17 23:29:23 +0200

    created a basic sign up that hashes the password

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,20 +1,23 @@
 const User = require("../schemas");
+const bcrypt = require('bcrypt'); 
+
 module.exports = async (req, res) => {
+  try {
     const { 
-        username,
-        password, 
-        first_name, 
-        last_name
+      username,
+      password, 
+      first_name, 
+      last_name
     } = req.body;
-    try {
-      const user = await User.create({ 
-        username,
-        password, 
-        first_name, 
-        last_name
-       });
-      res.status(201).json({ data: user });
-    } catch (error) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ 
+      username,
+      password: hashedPassword, 
+      first_name, 
+      last_name
+    });
+    res.status(201).json({ data: user });
+  } catch (error) {
         res.status(400).json({ error });
-    }
-  };
+  }
+};
