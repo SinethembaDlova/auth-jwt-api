51e949ac00477b9a933a949951a3813b8ab98dcb
Sinethemba <sinethemba@wealthmigrate.com>
2022-08-17 23:46:56 +0200

    created a basic sign in with comparing passwords

--- src/controllers/signin.js
+++ src/controllers/signin.js
@@ -1,17 +1,20 @@
 const User = require("../schemas");
+const bcrypt = require('bcrypt'); 
 
 module.exports = async (req, res) => {
     const { 
         username,
         password
     } = req.body;
+    const user = await User.find({ username });
+    if (user.length === 0) {
+        return res.status(400).json({ data: [] })
+    }
     try {
-      const user = await User.create({ 
-        username,
-        password
-       });
-      res.status(201).json({ data: user });
+        if (bcrypt.compare(password, user.password)){
+            res.status(200).json({ data: user })
+        }
     } catch (error) {
         res.status(400).json({ error });
-    }
+      }
   };
