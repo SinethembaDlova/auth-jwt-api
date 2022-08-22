db959bced713d635cf18e4c176856e68360b2477
Sinethemba <sinethemba@wealthmigrate.com>
2022-08-23 00:09:23 +0200

    created try catch in signin

--- src/controllers/signin.js
+++ src/controllers/signin.js
@@ -4,18 +4,23 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res) => {
+  try {
     const { 
-        username,
-        password
+      username,
+      password
     } = req.body;
-    const user = await User.find({ username, });
+    const user = await User.find({ username });
+    console.log('user: ', user);
     if (user.length <= 0) {
-        return res.status(204).json({ data: [] })
+      return res.status(204).json({ data: [] })
     } else {
       if (await bcrypt.compare(password, user[0].password)){
         const id = user[0]._id;
         const access_token = await jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET)
         res.status(200).json({ data: user, access_token })
       }
-    } 
+    }  
+  } catch (error) {
+    res.status(400).json({ error });
+  }
   };
